90b4960936d848ceef173e43bbd87ade27735234
DimaErm124 <dimaermakov@ro.ru>
2021-03-02 09:08:33 +0300

    All tasks completed

---
+++ 02-arrays-and-methods/ArraysAndMethods/ArrayLibrary/OneDimensionalIntegerArray.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLibrary
+{
+    public class OneDimensionalIntegerArray: IntegerArray
+    {
+        private int _n;
+
+        public int[] Array { get; private set; }
+
+        public OneDimensionalIntegerArray(int n, int maxRandomValue):base(maxRandomValue)
+        {
+            _n = n;
+        }
+
+        public override void GenerateArray()
+        {
+            Array = new int[_n];
+
+            Random random = new Random();
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Array[i] = random.Next(-MaxRandomValue, MaxRandomValue);
+            }
+        }
+
+        public int FindMinValue()
+        {
+            int maxValue = Array[0];
+
+            for (int i = 1; i < Array.Length; i++)
+            {
+                if (maxValue < Array[i])
+                {
+                    maxValue = Array[i];
+                }
+            }
+
+            return maxValue;
+        }
+
+        public int FindMaxValue()
+        {
+            int minValue = Array[0];
+
+            for (int i = 1; i < Array.Length; i++)
+            {
+                if (minValue > Array[i])
+                {
+                    minValue = Array[i];
+                }
+            }
+
+            return minValue;
+        }
+
+        public void SortArray()
+        {
+            int arr;
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                for (int j = 0; j < Array.Length - 1; j++)
+                {
+                    if (Array[j] > Array[j + 1])
+                    {
+                        arr = Array[j];
+                        Array[j] = Array[j + 1];
+                        Array[j + 1] = arr;
+                    }
+                }
+            }
+        }
+
+        public void SortAndGetMinAndMaxValues(out int maxValue, out int minValue)
+        {
+            maxValue = FindMaxValue();
+            minValue = FindMinValue();
+            SortArray();
+        }
+    }
+}
---
+++ 02-arrays-and-methods/ArraysAndMethods/Task3/Program.cs
@@ -0,0 +1,92 @@
+﻿using ArrayLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxRandomValue = 10;
+            int oneSize;            
+            int[] array;
+
+            int sumOfNonNegativeElements;
+
+            Console.WriteLine("Enter one size array:");
+            oneSize = EnterSizeArray();
+
+            OneDimensionalIntegerArray integerArray =
+                new OneDimensionalIntegerArray(oneSize, maxRandomValue);
+
+            integerArray.GenerateArray();
+
+            array = integerArray.Array;
+
+            Console.WriteLine("\nOriginal array:");
+            PrintArray(array);
+
+            sumOfNonNegativeElements = GetSumOfNonNegativeElements(array);
+
+            Console.WriteLine("\nSum of non negative elements:");
+            Console.WriteLine(sumOfNonNegativeElements);
+        }
+
+        public static int GetSumOfNonNegativeElements(int[] array)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += IsPositive(array, i);
+            }
+
+            return sum;
+        }
+
+        public static int IsPositive(int[] array, int i)
+        {
+            if (array[i] > 0)
+            {
+                return array[i];
+            }
+
+            return 0;
+        }
+
+        public static int EnterSizeArray()
+        {
+            int size = 0;
+            string stringSize;
+
+            do
+            {
+                stringSize = Console.ReadLine();
+            }
+            while (!IsPositiveInteger(stringSize, ref size));
+
+            return size;
+        }
+
+        public static bool IsPositiveInteger(string stringSize, ref int size)
+        {
+            if (int.TryParse(stringSize, out size))
+                if (size > 0)
+                    return true;
+
+            return false;
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
