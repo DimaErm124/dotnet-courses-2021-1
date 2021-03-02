bab6244eef82c93245178c1308d0f3421107dbeb
DimaErm124 <dimaermakov@ro.ru>
2021-03-03 00:16:47 +0300

    tasks fix

---
+++ 03-strings/Strings/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+
+            Console.WriteLine("Enter string:");
+            inputString = Console.ReadLine();
+
+        }   
+    }
+}
---
+++ 03-strings/Strings/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstInputString;
+            string secondInputString;
+
+            string changedString;
+
+            Console.WriteLine("Enter first string:");
+            firstInputString = Console.ReadLine();
+
+            Console.WriteLine("Enter second string:");
+            secondInputString = Console.ReadLine();
+
+            changedString = ReplaceAllSymbols(firstInputString, secondInputString);
+
+            Console.WriteLine("Changed string:");
+            Console.WriteLine(changedString);
+        }
+
+        public static string ReplaceAllSymbols(string firstInputString, string secondInputString)
+        {
+            string str=firstInputString;
+
+            foreach(char el in secondInputString)
+            {
+                string duplicateSymbol = el.ToString();
+                str = str.Replace(duplicateSymbol, new string(el,2));
+            }
+
+            return str;
+        }
+    }
+}
