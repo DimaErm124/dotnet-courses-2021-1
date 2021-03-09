647ebd99fab2543815e6813d3ae7a08a99d14fee
DimaErm124 <dimaermakov@ro.ru>
2021-03-10 00:02:33 +0300

    strings and ref-types is done

--- 04-reference-types/ReferenceTypes/Task2/Program.cs
+++ 04-reference-types/ReferenceTypes/Task2/Program.cs
@@ -17,30 +17,4 @@ namespace Task2
             Console.WriteLine("Area of round ({0},{1})          - {2}", round.X, round.Y, Math.Round(round.Area, 3));
         }
     }
-
-    public class Round
-    {
-        private int _radius;
-
-        public int X { get; set; }
-
-        public int Y { get; set; }
-
-        public int Radius 
-        { 
-            get { return _radius; }
-            private set { if (value <= 0) { throw new Exception("Radius must be greater than zero!"); }   _radius = value; } 
-        }
-
-        public double Circumference { get { return 2 * Math.PI * _radius; } }
-
-        public double Area { get { return Math.PI * Math.Pow(_radius, 2); } }
-
-        public Round(int x, int y, int radius)
-        {
-            X = x;
-            Y = y;
-            Radius = radius;
-        }
-    }
 }
--- 04-reference-types/ReferenceTypes/Task4/Program.cs
+++ 04-reference-types/ReferenceTypes/Task4/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Task4
@@ -18,52 +16,6 @@ namespace Task4
             Console.WriteLine(" {0} - {1} = {2}", myString1, myString2, myString1 - myString2);
             Console.WriteLine(" {0} == {1} = {2}", myString1, myString2, myString1 == myString2);
             Console.WriteLine(" {0} != {1} = {2}", myString1, myString2, myString1 != myString2);
-        }
-    }
-
-    public class MyString
-    {
-        private char[] _charArray;
-
-        public MyString()
-        {
-            _charArray = string.Empty.ToCharArray();
-        }
-
-        public MyString(string sourceString)
-        {
-            _charArray = sourceString.ToCharArray();
-        }
-
-        public MyString(char[] charArray)
-        {
-            _charArray = charArray;
-        }
-
-        public static MyString operator +(MyString ms1, MyString ms2)
-        {
-            return new MyString(ms1._charArray.Concat(ms2._charArray).ToArray());
-        }
-
-        public static MyString operator -(MyString ms1, MyString ms2)
-        {
-            var regex = new Regex(ms2.ToString());
-            return new MyString(regex.Replace(ms1.ToString(), "",1));
-        }
-
-        public static bool operator ==(MyString ms1, MyString ms2)
-        {
-            return ms1._charArray.Equals(ms2._charArray);
-        }
-
-        public static bool operator !=(MyString ms1, MyString ms2)
-        {
-            return !ms1._charArray.Equals(ms2._charArray);
-        }
-
-        public override string ToString()
-        {
-            return new string(_charArray);
         }
     }
 }
