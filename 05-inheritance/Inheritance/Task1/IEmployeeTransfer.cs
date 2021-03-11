9a6c4e25df4e46bd129ff51677e9070ac58cc5c3
DimaErm124 <dimaermakov@ro.ru>
2021-03-12 00:51:02 +0300

    inheritance is done

---
+++ 05-inheritance/Inheritance/Task1/IEmployeeTransfer.cs
@@ -0,0 +1,12 @@
+﻿namespace Task1
+{
+    public interface IEmployeeTransfer
+    {
+        string Department { get; }
+
+        string Title { get; }
+
+        double Salary { get; }
+
+    }
+}
