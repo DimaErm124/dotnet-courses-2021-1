5f56c171e32568304a9d68493539f2f54c220e26
DimaErm124 <dimaermakov@ro.ru>
2021-03-22 23:33:01 +0300

    !DelEvents:task3

---
+++ 09-delegates-and-events/DelegatesAndEvents/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using SortLibrary;
+using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new string[] { "aabb", "aaabb", "a", "b", "abbbbbb", "baaa", "aaaba" };
+
+            var strArray = new StringArrayCustomSort(arr, CompareStrings);
+
+            var sortThread = strArray.SortAsync();
+
+            sortThread.Start();
+            strArray.Sort();
+
+            Console.WriteLine(strArray);
+        }
+
+        public static int CompareStrings(string a, string b)
+        {
+            return a.Length - b.Length;
+        }
+
+    }
+}
