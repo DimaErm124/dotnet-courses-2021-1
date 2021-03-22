0ca092e880222253ec10d75a00bbff5317e68be9
DimaErm124 <dimaermakov@ro.ru>
2021-03-22 11:19:53 +0300

    DelegateAndEvents: task1 is done

---
+++ 09-delegates-and-events/DelegatesAndEvents/Task2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        public event EventHandler<PersonEventArgs> inOffice;
+
+        public event EventHandler outOffice;
+
+        static void Main(string[] args)
+        {
+            Person jack = new Person("Джэк");
+            Person john = new Person("Джон");
+            Person sam = new Person("Сэм");
+            Person fred = new Person("Фред");
+        }
+
+        public void GoIn(Person person)
+        {
+            inOffice(person, new PersonEventArgs(DateTime.Now));
+            inOffice += person.Greeting;
+        }
+
+        public void GoOut(Person person)
+        {
+            outOffice(person, new EventArgs());
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void Greeting(Person person, PersonEventArgs personEventArgs)
+        {
+            var hour = personEventArgs.ArrivalTime.Hour;
+
+            var greetingString = string.Empty;
+
+            if (hour < 12)
+            {
+                greetingString += "Доброе утро, ";
+            }
+            if (hour >= 12 || hour < 17)
+            {
+                greetingString += "Добрый день, ";
+            }
+            if (hour >= 17)
+            {
+                greetingString += "Добрый вечер, ";
+            }
+
+            var str = greetingString + person.Name;
+        }
+
+        public string Parting(Person person)
+        {
+            return "Пока, " + person.Name;
+        }
+
+    }
+
+    public class PersonEventArgs : EventArgs
+    {
+        public DateTime ArrivalTime { get; set; }
+
+        public PersonEventArgs(DateTime dateTime)
+        {
+            ArrivalTime = dateTime;
+        }
+    }
+}
