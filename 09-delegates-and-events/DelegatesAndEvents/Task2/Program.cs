a465c261c202f85df2f52c4ce9fccc88067ee3dc
DimaErm124 <dimaermakov@ro.ru>
2021-03-22 20:26:12 +0300

    DelEvents: task 1,2 are done

--- 09-delegates-and-events/DelegatesAndEvents/Task2/Program.cs
+++ 09-delegates-and-events/DelegatesAndEvents/Task2/Program.cs
@@ -7,76 +7,40 @@ using System.Threading.Tasks;
 namespace Task2
 {
     class Program
-    {
-        public event EventHandler<PersonEventArgs> inOffice;
-
-        public event EventHandler outOffice;
-
+    {       
         static void Main(string[] args)
         {
-            Person jack = new Person("Джэк");
-            Person john = new Person("Джон");
-            Person sam = new Person("Сэм");
-            Person fred = new Person("Фред");
-        }
+            var people = GetPeople("Джэк", "Джон", "Сэм", "Фред", "Боб", "Ричард");
 
-        public void GoIn(Person person)
-        {
-            inOffice(person, new PersonEventArgs(DateTime.Now));
-            
-        }
+            var office = new Office();
 
-        public void GoOut(Person person)
-        {
-            outOffice(person, new EventArgs());
-        }
-    }
+            var consoleDisplayer = new ConsoleDisplayer();
 
-    public class Person
-    {
-        public string Name { get; set; }
-
-        public Person(string name)
-        {
-            Name = name;
-        }
-
-        public void Greeting(Person person, PersonEventArgs personEventArgs)
-        {
-            var hour = personEventArgs.ArrivalTime.Hour;
-
-            var greetingString = string.Empty;
-
-            if (hour < 12)
+            foreach(var person in people)
             {
-                greetingString += "Доброе утро, ";
-            }
-            if (hour >= 12 || hour < 17)
-            {
-                greetingString += "Добрый день, ";
-            }
-            if (hour >= 17)
-            {
-                greetingString += "Добрый вечер, ";
+                Console.WriteLine("[В офис пришел {0}]", person.Name);
+                office.Come(person, consoleDisplayer);
+                Console.WriteLine();
             }
 
-            var str = greetingString + person.Name;
+            foreach (var person in people)
+            {
+                Console.WriteLine("[Офис покинул {0}]", person.Name);
+                office.Leave(person, consoleDisplayer);
+                Console.WriteLine();
+            }
         }
 
-        public string Parting(Person person)
+        public static List<Person> GetPeople(params string[] names)
         {
-            return "Пока, " + person.Name;
-        }
+            var people = new List<Person>();
 
-    }
+            foreach (var name in names)
+            {
+                people.Add(new Person(name));
+            }
 
-    public class PersonEventArgs : EventArgs
-    {
-        public DateTime ArrivalTime { get; set; }
-
-        public PersonEventArgs(DateTime dateTime)
-        {
-            ArrivalTime = dateTime;
+            return people;
         }
     }
 }
