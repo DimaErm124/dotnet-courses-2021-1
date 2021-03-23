3a6c3157b83b33f5d07af1bf000b37258e15f47c
DimaErm124 <dimaermakov@ro.ru>
2021-03-23 23:31:13 +0300

    DotnetBasics is done

---
+++ 10-dotnet-basics/DotnetBasics/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using MathLibrary;
+using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 3;
+            double x = 2;
+            double y = 4;
+
+            Console.WriteLine("{0}! = {1}", number, MathFunctions.Factorial(number));
+            Console.WriteLine("{0}^{1} = {2}", x, y, MathFunctions.Power(x, y));
+        }
+    }
+}
---
+++ 10-dotnet-basics/DotnetBasics/Task2/IEmployeeTransfer.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2
+{
+    public interface IEmployeeTransfer
+    {
+        string Department { get; }
+
+        string Title { get; }
+
+        double Salary { get; }
+
+    }
+}
