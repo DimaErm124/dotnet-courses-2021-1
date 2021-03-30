5bcc1d5ced45c6fb5bca1b5fb46052c43ad31af7
DimaErm124 <dimaermakov@ro.ru>
2021-03-30 14:25:30 +0300

    Files are done (fileSystemWatch)

---
+++ 11-files/Files/Task2/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    public class Logger
+    {
+        private Log _log;
+
+        private FileSystemWatcher _watcher;
+
+        public Logger(string catalog, string pattern)
+        {
+            _log = new Log(catalog, pattern);
+            WatcherInit(catalog, pattern);
+        }
+
+        private void WatcherInit(string catalog, string pattern)
+        {
+            _watcher = new FileSystemWatcher(catalog);
+
+            _watcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.LastWrite;
+
+            _watcher.Changed += _watcher_Changed;
+            _watcher.Filter = pattern;
+            _watcher.EnableRaisingEvents = false;
+        }
+
+        public void LogEnable()
+        {
+            _watcher.EnableRaisingEvents = !_watcher.EnableRaisingEvents;
+        }
+
+        public void Pull(DateTime dateTime)
+        {
+            _log.Pull(dateTime);
+        }
+
+        private void _watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            _log.Push();
+        }
+    }
+}
